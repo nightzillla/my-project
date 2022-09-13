51104de1daedd9598fdf5679d1c35126f9a738ef
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-09-13 17:57:28 -0500

    testing code
    working on video

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-// import React from "react";
-import React, {useState, useEffect} from "react";
-import Clock from './components/Clock'
-import Video from './video/smoke3.mp4'
+import React from "react";
+// import React, {useState, useEffect} from "react";
 import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavbarComp from './components/NavbarComp';
 
-function App() {
-    const [isLoading, setIsLoading] = useState(true);
-    useEffect(() => {
-        setTimeout(() => {
-            setIsLoading(false)
-        }, 8000); 
-    })
+function App () {
     return (
-        <div>
-            {isLoading===true?
-            <Video/>: 
-            <hidden/>
-            }      
+        <div className="App">
+            Hello
         </div>
     );
 }
+
+
+
+// function App() { //testing code for loader 
+//     const [isLoading, setIsLoading] = useState(true);
+//     useEffect(() => {
+//         setTimeout(() => {
+//             setIsLoading(false)
+//         }, 8000); 
+//     })
+//     return (
+//         <div>
+//             {isLoading===true?
+//             <Video/>: 
+//             <hidden/>
+//             }      
+//         </div>
+//     );
+// }
 // const Video = () => {
 //     return (
 //       <div className="container">
