303ad0da8db9b9c84c0863a45af20e6128dd8d25
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-11-17 17:52:02 -0600

    added video as component

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Smoke from '../video/smoke3.mp4'
+import NavBar from './NavBar'
+
+function HomePage() {
+  return (
+    <>
+    <div className="loading-wrapper">
+      <div className="video-container">
+        <video autoPlay muted loop className="smoke">
+          <source src={Smoke} />
+        </video>
+      </div>
+      </div>
+      <div className="section">
+        <h1>nightzillla website coming soon</h1>
+        <NavBar/>
+      <div className="loader"> 
+        <span className="loader__element"></span>
+        <span className="loader__element"></span>
+        <span className="loader__element"></span>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default HomePage
