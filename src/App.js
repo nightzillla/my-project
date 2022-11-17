d25c2d7d7003cf6a3c5ef333caf08997a812f28e
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-11-17 15:29:10 -0600

    updating code in react

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 // import React, {useState, useEffect} from "react";
 import "./App.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import NavbarComp from './components/NavbarComp';
+
 
 function App () {
     return (
@@ -11,40 +11,5 @@ function App () {
         </div>
     );
 }
-
-
-
-// function App() { //testing code for loader 
-//     const [isLoading, setIsLoading] = useState(true);
-//     useEffect(() => {
-//         setTimeout(() => {
-//             setIsLoading(false)
-//         }, 8000); 
-//     })
-//     return (
-//         <div>
-//             {isLoading===true?
-//             <Video/>: 
-//             <hidden/>
-//             }      
-//         </div>
-//     );
-// }
-// const Video = () => {
-//     return (
-//       <div className="container">
-//         <video autoPlay playsInline muted src={video} />
-//       </div>
-//     );
-//   };
-//   function App() {
-//     return (
-//       <div className="App">
-//         <div className="app-container">
-//           <Video />
-//         </div>
-//       </div>
-//     );
-//   }
 
 export default App;
