204f346a53bc39f37e8da3436fa59b1b68e0d091
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-11-22 12:07:05 -0600

    updating github url

--- src/Pages/Cohort/Cohort.js
+++ src/Pages/Cohort/Cohort.js
@@ -38,32 +38,32 @@ function Cohort() {
         <img className='space-wallpaper'src={spaceWallpaper}/>
       </div> {/* end of weywot */}
       <div className="NFTJavier Container grid grid-flow-row">
-        <NFTcard className="NFTcards"img={Javier} name={"Javier"} href={"www.google.com"}/>
+        <NFTcard className="NFTcards"img={Javier} name={"Javier"} href={"https://github.com/jruedas1"}/>
       </div>
       <div className="NFTcardHolders flex flex-wrap justify-between ">
         <NFTcard className="NFTcards"img={Danny} name={"nightzillla"} href={"https://github.com/nightzillla"}/>
-        <NFTcard className="NFTcards mt-6"img={Stepbro} name={"x"} href={"www.google.com"}/>
-        <NFTcard className="NFTcards"img={broMason} name={"Mason"} href={"www.google.com"}/>
-        <NFTcard className="NFTcards"img={David} name={"David"} href={"www.google.com"}/>
-        <NFTcard className="NFTcards"img={Jason} name={"Jason"} href={"www.google.com"}/>
+        <NFTcard className="NFTcards mt-6"img={Stepbro} name={"x"} href={"https://github.com/Exabiier"}/>
+        <NFTcard className="NFTcards"img={broMason} name={"Mason"} href={"https://github.com/WoodyMas"}/>
+        <NFTcard className="NFTcards"img={David} name={"David"} href={"https://github.com/Lara5595"}/>
+        <NFTcard className="NFTcards"img={Jason} name={"Jason"} href={"https://github.com/merrellj-codeup"}/>
         <NFTcard className="NFTcards"img={Fernando} name={"Fernando"} href={"https://github.com/fernandolopez32"}/>
-        <NFTcard className="NFTcards"img={Arron} name={"AAron"} href={"www.google.com"}/>
-        <NFTcard className="NFTcards"img={Deshawn} name={"Deshawn"} href={"www.google.com"}/>
-        <NFTcard className="NFTcards"img={Frances} name={"Frances"} href={"www.google.com"}/>
-        <NFTcard className="NFTcards"img={John} name={"John"} href={"www.google.com"}/>
-        <NFTcard className="NFTcards"img={naz} name={"naz"} href={"www.google.com"}/>
-        <NFTcard className="NFTcards"img={Stephen} name={"Stephen"} href={"www.google.com"}/>
-        <NFTcard className="NFTcards"img={val} name={"val"} href={"www.google.com"}/>
-        <NFTcard className="NFTcards"img={Jack} name={"jack"} href={"www.google.com"}/>
-        <NFTcard className="NFTcards"img={rodrigo} name={"Rodrigo"} href={"www.google.com"}/>
-        <NFTcard className="NFTcards"img={Ceaser} name={"Ceaser"} href={"www.google.com"}/>
-        <NFTcard className="NFTcards"img={Daniel} name={"Daniel"} href={"www.google.com"}/>
-        <NFTcard className="NFTcards"img={jasmine} name={"jasmine"} href={"www.google.com"}/>
-        <NFTcard className="NFTcards"img={Rich} name={"Rich"} href={"www.google.com"}/>
-        <NFTcard className="NFTcards"img={Adien} name={"Adien"} href={"www.google.com"}/>
+        <NFTcard className="NFTcards"img={Arron} name={"AAron"} href={"https://github.com/aarmorris"}/>
+        <NFTcard className="NFTcards"img={Deshawn} name={"Deshawn"} href={"https://github.com/MarquisTheCoder"}/>
+        <NFTcard className="NFTcards"img={Frances} name={"Frances"} href={"https://github.com/francesperez"}/>
+        <NFTcard className="NFTcards"img={John} name={"John"} href={"https://github.com/jdpedrotti"}/>
+        <NFTcard className="NFTcards"img={naz} name={"naz"} href={"https://github.com/NazarethBriseno"}/>
+        <NFTcard className="NFTcards"img={Stephen} name={"Stephen"} href={"https://github.com/Pfirewire"}/>
+        <NFTcard className="NFTcards"img={val} name={"val"} href={"https://github.com/vcanizales"}/>
+        <NFTcard className="NFTcards"img={Jack} name={"jack"} href={"https://github.com/jackbgraham"}/>
+        <NFTcard className="NFTcards"img={rodrigo} name={"Rodrigo"} href={"https://github.com/RodrigoM09"}/>
+        <NFTcard className="NFTcards"img={Ceaser} name={"Ceaser"} href={"https://github.com/Clopez-25"}/>
+        <NFTcard className="NFTcards"img={Daniel} name={"Daniel"} href={"https://github.com/danielname"}/>
+        <NFTcard className="NFTcards"img={jasmine} name={"Jazmine"} href={"https://github.com/JazHud"}/>
+        <NFTcard className="NFTcards"img={Rich} name={"Rich"} href={"https://github.com/richy088"}/>
+        <NFTcard className="NFTcards"img={Adien} name={"Adien"} href={"https://github.com/AidenLuce"}/>
         <NFTcard className="NFTcards"img={Caleb} name={"Caleb"} href={"https://github.com/theCEO1999"}/>
-        <NFTcard className="NFTcards"img={Test} name={"Test"} href={"www.google.com"}/>
-        <NFTcard className="NFTcards"img={Gonzo} name={"Gonzo"} href={"www.google.com"}/>
+        <NFTcard className="NFTcards"img={Test} name={"Test"} href={"https://github.com/ttekie"}/>
+        <NFTcard className="NFTcards"img={Gonzo} name={"Gonzo"} href={"https://github.com/gonzaloovalle"}/>
       </div>
       <div>
       </div>
